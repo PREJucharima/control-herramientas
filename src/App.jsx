54dea9f7e54dd291ca69293235b823412d6a825e
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-09 17:40:50 -0500

    integra restauración de sesión y verificación de token

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import { AppTheme } from "./config/theme/AppTheme";
+import { useAuthStore } from "./modules/auth/states/authStore";
 import { AppRouter } from "./routes/AppRouter";
 
 function App() {
+  const restoreSession = useAuthStore(state => state.restoreSession);
+  const checkTokenExpiration = useAuthStore(state => state.checkTokenExpiration);
+
+  useEffect(() => {
+    restoreSession();
+
+    const interval = setInterval(() => {
+      checkTokenExpiration();
+    }, 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <AppTheme>
