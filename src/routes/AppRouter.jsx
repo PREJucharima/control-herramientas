78542c0ff3ac4621a9632aed9773e8e020b8a7cf
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-09 17:36:40 -0500

    agrega configuración para las rutas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,13 +1,22 @@
 import { Navigate, Route, Routes } from "react-router";
 import { AuthRoutes } from "../modules/auth/routes/AuthRoutes";
+import { useCheckAuth } from "../modules/auth/hooks/useCheckAuth";
+import { HomePage } from "../modules/home/pages/HomePage"
 
 export const AppRouter = () => {
+  const { status } = useCheckAuth();
+
+  if (status === "checking") {
+    return <><p>Loading...</p> </>
+  }
+
   return (
     <Routes>
-      <>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<HomePage />} />
+      ) : (
         <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/*" element={<Navigate to="/auth/login" />} />
-      </>
+      )}
 
       <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
