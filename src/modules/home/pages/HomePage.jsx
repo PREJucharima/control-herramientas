a9af36ff4a37e5882b5aa9d162357506d9f4661b
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-09 18:20:45 -0500

    agrega cierre de sesión

--- src/modules/home/pages/HomePage.jsx
+++ src/modules/home/pages/HomePage.jsx
@@ -2,20 +2,25 @@ import { googleLogout } from '@react-oauth/google';
 import { Button, Container } from '@mui/material';
 import { useNavigate } from 'react-router';
 import { useAuthStore } from '../../auth/states/authStore';
+import { Navigate } from "react-router";
 
 export const HomePage = () => {
   const logout = useAuthStore(state => state.logout);
+  const user = useAuthStore(state => state.user)
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    googleLogout();      // cierra sesión de Google
-    logout();            // borra estado local
-    navigate('/auth/login'); // redirige al login
+    googleLogout();
+    logout();
+    navigate('/auth/login');
   };
+
+  console.log(user)
+  if (!user) return <Navigate to="/auth/login" />;
 
   return (
     <Container>
-      <h1>HomePage</h1>
+      <h1>Bienvenido {user.nombre_usuario} {user.apellido_usuario}</h1>
       <Button variant="contained" onClick={handleLogout}>
         Cerrar sesión
       </Button>
