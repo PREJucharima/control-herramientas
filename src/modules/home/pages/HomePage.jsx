7a5d72e50ce27eaf9ce732ec4cd0dde1e55c2b87
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-11 18:55:45 -0500

    consume service para obtener la navegacion

--- src/modules/home/pages/HomePage.jsx
+++ src/modules/home/pages/HomePage.jsx
@@ -3,11 +3,18 @@ import { Button, Container } from '@mui/material';
 import { useNavigate } from 'react-router';
 import { useAuthStore } from '../../auth/states/authStore';
 import { Navigate } from "react-router";
+import { RootLayout } from '../../core/layout/RootLayout';
+import { useEffect, useState } from 'react';
+import { getNavigation } from '../services/getNavigation';
 
 export const HomePage = () => {
   const logout = useAuthStore(state => state.logout);
-  const user = useAuthStore(state => state.user)
+  const user = useAuthStore(state => state.user);
+  const token = useAuthStore(state => state.token);
+  const [menus, setMenus] = useState([]);
   const navigate = useNavigate();
+
+  console.log({user, token})
 
   const handleLogout = () => {
     googleLogout();
@@ -15,15 +22,30 @@ export const HomePage = () => {
     navigate('/auth/login');
   };
 
-  console.log(user)
+  useEffect(() => {
+    const fetchNavigation = async () => {
+      try {
+        const data = await getNavigation({ token, rol_id: user.rol_id });
+        setMenus(data);
+      } catch (error) {
+        console.error("Error cargando menús:", error);
+      }
+    };
+
+    if (user && token) fetchNavigation();
+  }, [user, token]); 
+
   if (!user) return <Navigate to="/auth/login" />;
 
   return (
-    <Container>
-      <h1>Bienvenido {user.nombre_usuario} {user.apellido_usuario}</h1>
-      <Button variant="contained" onClick={handleLogout}>
-        Cerrar sesión
-      </Button>
-    </Container>
+    <RootLayout>
+      <Container>
+        <h1>Bienvenido {user.nombre_usuario} {user.apellido_usuario}</h1>
+        <pre>{JSON.stringify(menus, null, 2)}</pre>
+        <Button variant="contained" onClick={handleLogout}>
+          Cerrar sesión
+        </Button>
+      </Container>
+    </RootLayout>
   );
 };
