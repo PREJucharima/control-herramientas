ac2fe3e562d4691e66efc555961c2e4e544f9f08
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-22 19:07:58 -0500

    corrige las rutas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,29 +1,29 @@
 import { Navigate, Route, Routes } from "react-router";
-import { AuthRoutes } from "../modules/auth/routes/AuthRoutes";
-import { useCheckAuth } from "../modules/auth/hooks/useCheckAuth";
-import { HomePage } from "../modules/home/pages/HomePage";
 import { CircularProgress } from "@mui/material";
+import { AuthRoutes } from "@/auth/routes/AuthRoutes";
+import { useCheckAuth } from "@/auth/hooks/useCheckAuth";
+import { HomePage } from "@/modules/home/pages/HomePage";
 
 export const AppRouter = () => {
   const { status } = useCheckAuth();
 
   if (status === "checking") {
-    return (
-      <>
-        <CircularProgress />
-      </>
-    );
+    return <CircularProgress />;
   }
 
   return (
     <Routes>
       {status === "authenticated" ? (
-        <Route path="/*" element={<HomePage />} />
+        <>
+          <Route path="/*" element={<HomePage />} />
+          <Route path="/auth/*" element={<Navigate to="/" />} />
+        </>
       ) : (
-        <Route path="/auth/*" element={<AuthRoutes />} />
+        <>
+          <Route path="/auth/*" element={<AuthRoutes />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
       )}
-
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
