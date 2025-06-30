40d0204a421135771034c1b40eefe5f1b16d506c
Julio Ucharima <julio.ucharima@precision.tech>
2025-06-29 20:55:25 -0500

    corregir mapeo de iconos

--- src/utils/iconMap.js
+++ src/utils/iconMap.js
@@ -1,15 +1,16 @@
 import duotone from "@/icons/duotone";
+import LockOutlined from "@/icons/LockOutlined";
 
 export const iconMap = {
-  "bi-speedometer2": duotone.PersonChalkboard,
+  "bi-speedometer2": duotone.Dashboard,
   "bi-book-half": duotone.DataTable,
   "bi-box-seam": duotone.DataTable,
-  "bi-boxes": duotone.DataTable,
-  "bi-arrows-move": duotone.Ecommerce,
+  "bi-boxes": duotone.PersonChalkboard,
+  "bi-arrows-move": duotone.DiagramProject,
   "bi-file-earmark-bar-graph-fill": duotone.RectangleCirclePlus,
-  "bi-shield-lock-fill": duotone.DiagramProject,
+  "bi-shield-lock-fill": LockOutlined,
   "bi-person-badge-fill": duotone.UserRole,
   "bi-people-fill": duotone.UserList,
   "bi-map": duotone.Map,
-  "bi-gear-wide-connected": duotone.LayerGroup,
+  "bi-gear-wide-connected": duotone.Settings,
 };
