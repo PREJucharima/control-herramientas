b2e14a73f0bdf319e6f7e9c481c23d92613404f9
Julio Ucharima <julio.ucharima@precision.tech>
2025-09-15 18:13:57 -0500

    agregar custom hook para hacer el fetch de itemsbyMaestro

---
+++ src/modules/items/hooks/useFetchItemsByMaestro.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useItemsStore } from "../states/itemsStore";
+import { getItemsByMaestro } from "../services/getItemsByMaestro";
+
+export const useFetchItemsByMaestro = (id) => {
+  const setItemsByMaestro = useItemsStore((state) => state.setItemsByMaestro);
+  const { itemsByMaestro } = useItemsStore();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const data = await getItemsByMaestro(id);
+        console.log("Ítems cargados:", data);
+        setItemsByMaestro(data);
+      } catch (err) {
+        console.error("Error cargando ítems:", err);
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (id) fetch();
+    else setIsLoading(false);
+  }, [id, setItemsByMaestro]);
+
+  return { itemsByMaestro, isLoading, error };
+};
