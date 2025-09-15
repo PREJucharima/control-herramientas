5ea2b44334a66d0f7ec5b3793b652ab2a47b5bc9
Julio Ucharima <julio.ucharima@precision.tech>
2025-09-14 19:52:05 -0500

    configurar LocalizationProvider con AdapterDayjs para MUI DatePickers

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import App from "./App.jsx";
 import "simplebar-react/dist/simplebar.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </LocalizationProvider>
   </StrictMode>
 );
