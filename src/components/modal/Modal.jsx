1542b8eaa7f169eca8f1b88ff0d909400b670f5c
Julio Ucharima <julio.ucharima@precision.tech>
2025-09-17 17:43:54 -0500

    eliminar comentarios

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,17 +1,13 @@
-import MuiModal from '@mui/material/Modal'; // STYLED COMPONENT
+import MuiModal from "@mui/material/Modal";
 
-import { StyledScrollbar, Wrapper } from './styles'; // ===========================================================================
+import { StyledScrollbar, Wrapper } from "./styles";
 
-// ===========================================================================
-export default function Modal({
-  children,
-  open,
-  sx,
-  handleClose
-}) {
-  return <MuiModal open={open} onClose={handleClose}>
+export default function Modal({ children, open, sx, handleClose }) {
+  return (
+    <MuiModal open={open} onClose={handleClose}>
       <Wrapper sx={sx}>
         <StyledScrollbar>{children}</StyledScrollbar>
       </Wrapper>
-    </MuiModal>;
+    </MuiModal>
+  );
 }
