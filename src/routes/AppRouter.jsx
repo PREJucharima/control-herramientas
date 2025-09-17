ed58293e5c4268d7c7c3e436328fb281d42bccea
Julio Ucharima <julio.ucharima@precision.tech>
2025-09-17 17:43:01 -0500

    implementar el status de autenticación del store a las rutas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,20 +1,34 @@
+// 1. React
+import { useEffect } from "react";
+
+// 2. Librerías Externas
 import { Navigate, Route, Routes } from "react-router";
 import { CircularProgress } from "@mui/material";
+
+// 3. Módulos Internos (con alias '@/')
 import { AuthRoutes } from "@/auth/routes/AuthRoutes";
-import { useCheckAuth } from "@/auth/hooks/useCheckAuth";
+import RootLayout from "@/layouts/RootLayout";
 import { HomePage } from "@/modules/home/pages/HomePage";
 import { MaestrosPage } from "@/modules/maestros/pages/MaestrosPage";
-import { CompanyBranchPage } from "@/modules/company-branch/pages/CompanyBranchPage";
-import { MyProfilePage } from "@/modules/my-profile/pages/MyProfilePage";
-import { ItemsPage } from "@/modules/items/pages/ItemsPage";
-import { AddNewItemPage } from "@/modules/items/pages/AddNewItemPage";
 import { AddNewMaestroPage } from "@/modules/maestros/pages/AddNewMaestroPage";
 import { EditMaestroPage } from "@/modules/maestros/pages/EditMaestroPage";
+import { ItemsPage } from "@/modules/items/pages/ItemsPage";
+import { AddNewItemPage } from "@/modules/items/pages/AddNewItemPage";
+import { CompanyBranchPage } from "@/modules/company-branch/pages/CompanyBranchPage";
+import { MyProfilePage } from "@/modules/my-profile/pages/MyProfilePage";
 
-import RootLayout from "@/layouts/RootLayout";
+// 4. Módulos Internos (relativos '../')
+import { useAuthStore } from "../auth/states/authStore";
 
 export const AppRouter = () => {
-  const { status } = useCheckAuth();
+  const status = useAuthStore((state) => state.status);
+  const restoreSession = useAuthStore((state) => state.restoreSession);
+
+  useEffect(() => {
+    restoreSession();
+  }, [restoreSession]);
+
+  console.log(status);
 
   if (status === "checking") {
     return <CircularProgress />;
@@ -29,11 +43,11 @@ export const AppRouter = () => {
             <Route path="inicio" element={<HomePage />} />
             <Route path="catalogos/maestros" element={<MaestrosPage />} />
             <Route
-              path="catalogos/agregar-maestro"
+              path="catalogos/maestros/agregar-maestro"
               element={<AddNewMaestroPage />}
             />
             <Route
-              path="/catalogos/:codigo/editar"
+              path="/catalogos/maestros/:codigo/editar"
               element={<EditMaestroPage />}
             />
             <Route
