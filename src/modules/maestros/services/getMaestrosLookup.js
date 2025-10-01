b99bf2374faa4b63a405af32abdf81b9a4b58723
Julio Ucharima <julio.ucharima@precision.tech>
2025-10-01 15:04:38 -0500

    actualizar endpoint para obtener el lookup de  los maestros

--- src/modules/maestros/services/getMaestrosLookup.js
+++ src/modules/maestros/services/getMaestrosLookup.js
@@ -1,7 +1,7 @@
 import { fetchWithAuth } from "@/auth/services/apiClient";
 
 export const getMaestrosLookup = async () => {
-  const response = await fetchWithAuth(`api/catalogos/lookup/`);
+  const response = await fetchWithAuth(`api/catalogos/lookup/tipos/`);
   const data = await response.json();
   return data;
 };
