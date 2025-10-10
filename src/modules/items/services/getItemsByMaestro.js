03c75f02678829ea8bd1244a840b936884e1a5b7
Julio Ucharima <julio.ucharima@precision.tech>
2025-10-09 19:20:00 -0500

    eliminar logs

--- src/modules/items/services/getItemsByMaestro.js
+++ src/modules/items/services/getItemsByMaestro.js
@@ -4,6 +4,5 @@ import { API_PATHS } from "@/config/apiPaths";
 export const getItemsByMaestro = async (id) => {
   const response = await fetchWithAuth(API_PATHS.lookup.itemsByMaster(id));
   const data = await response.json();
-  console.log("getItemsByMaestro data:", data);
   return data;
 };
