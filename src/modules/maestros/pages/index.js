c12abc46f745964af07c2602e1b65b1e81548111
Julio Ucharima <julio.ucharima@precision.tech>
2025-10-25 21:28:51 -0500

    agregar archivo de barril para la paginas del moduilo de maestros

---
+++ src/modules/maestros/pages/index.js
@@ -0,0 +1,3 @@
+export { default as AddNewMaestroPage } from "./AddNewMaestroPage";
+export { default as EditMaestroPage } from "./EditMaestroPage";
+export { default as MaestrosPage } from "./MaestrosPage";
